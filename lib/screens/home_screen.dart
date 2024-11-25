206c9089b42b143216d5f8cf7995205379a60a80
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-11-25 17:44:07 +0100

    AppBar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Hola")
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("HomeScreen"),
+        backgroundColor: Colors.green,
       ),
+      body: const Center(child: Text("Hola")),
     );
   }
-
 }
